30d7f4d6f0e5a19ee311b8893bb38ca53be7e277
Daiana <daianaerazo@gmail.com>
2023-02-11 14:58:39 -0300

    permitir cors

--- src/main/java/com/porfolio/Dai/Controller/CEducacion.java
+++ src/main/java/com/porfolio/Dai/Controller/CEducacion.java
@@ -25,7 +25,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/educacion")
-@CrossOrigin(origins = {"http://localhost:4200" , "https://porfolio-front.web.app"})
+@CrossOrigin(origins = {"http://localhost:4200" , "https://porfolio-front.web.app/"})
 public class CEducacion {
     @Autowired
     SEducacion sEducacion;
